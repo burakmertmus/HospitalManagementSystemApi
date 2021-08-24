b40d7c5b3e2f27b71c8b4a3ce480758cf931620a
Burak Mert Muş <burakmertmus@gmail.com>
2021-08-24 06:56:53 +0300

    Add project files.

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HospitalManagementSystemApi.Data;
+using HospitalManagementSystemApi.Models;
+using HospitalManagementSystemApi.RequestModels;
+
+namespace HospitalManagementSystemApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AppointmentsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Appointments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointment()
+        {
+            return await _context.Appointment.ToListAsync();
+        }
+
+        // GET: api/Appointments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appointment>> GetAppointment(int id)
+        {
+            var appointment = await _context.Appointment.FindAsync(id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return appointment;
+        }
+        
+        // PUT: api/Appointments/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppointment(int id, Appointment appointment)
+        {
+            if (id != appointment.appointment_id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appointment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Appointments
+        
+        [HttpPost]
+        public async Task<ActionResult<Appointment>> PostAppointment(AppointmentModel appointmentModel)
+        {
+            var appointment = new Appointment();
+            appointment.appointment_date = appointmentModel.appointment_date;
+            appointment.doc_id = appointmentModel.doc_id;
+            appointment.pat_id = appointmentModel.pat_id;
+            _context.Appointment.Add(appointment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAppointment", new { id = appointment.appointment_id }, appointment);
+        }
+
+        // DELETE: api/Appointments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appointment.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Appointment.Remove(appointment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return _context.Appointment.Any(e => e.appointment_id == id);
+        }
+    }
+}
---
+++ RequestModels/AppointmentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystemApi.RequestModels
+{
+    public class AppointmentModel
+    {
+        
+        public int doc_id { get; set; }
+        public int pat_id { get; set; }
+        
+        public DateTime appointment_date { get; set; }
+    }
+}
